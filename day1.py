8113b0ee731b872c541e71de3546cf90aa36c61e
Padhmawathy <177913149+Padhmawathy@users.noreply.github.com>
2025-09-19 12:39:53 +0530

    Delete day1.py

--- day1.py
+++
@@ -1,8 +0,0 @@
-def palindrome(i,j):
-	non_rev = str(i)
-	for iteration in range(1,j+1):
-		rev = non_rev[::-1]
-		sum = int(non_rev) + int(rev)
-		if str(sum) == str(sum[::-1]):
-			return iteration
-		non_rev = str(sum)
